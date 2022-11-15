3386e07474981bd253e91987874f136c6f02029f
Umair Abdullah <umaharabdullah@gmail.com>
2022-11-15 22:36:59 +0500

    Base URL

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://karwan-api.herokuapp.com" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export default API;
 
-// http://localhost:5000/
-// https://karwan-api.herokuapp.com
+// LOCAL HOST: http://localhost:5000/
+// HEROKU: https://karwan-api.herokuapp.com/
+// AMAZON AWS http://ec2-18-183-26-143.ap-northeast-1.compute.amazonaws.com:5000/
